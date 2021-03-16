f36729336bb36f2cb9fc1079af6cf1ee81793703
Ccourter11 <courtercalvin@yahoo.com>
2021-03-16 10:52:21 -0500

    got authentication set up, along with a few corner stone components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import { Train } from "./components/Train"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router >
+    <Train />
+   </Router >
   </React.StrictMode>,
   document.getElementById('root')
 );

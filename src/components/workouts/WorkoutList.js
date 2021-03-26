6741ff2ab5225e44ce8ce45030a0b6f6dfa008f1
Ccourter11 <courtercalvin@yahoo.com>
2021-03-26 14:37:40 -0500

    comments

--- src/components/workouts/WorkoutList.js
+++ src/components/workouts/WorkoutList.js
@@ -2,8 +2,6 @@ import React, { useEffect, useContext, useState } from "react"
 import { WorkoutCard } from "./WorkoutCard"
 import "./Workout.css"
 import { userStorageKey } from "../auth/authSettings"
-import Button from 'react-bootstrap/Button'
-
 
 // The useContext hook allows you to use data structures and functions that a parent provider,the WorkoutProvider, component exposes.
 // To start, you need to import the context object you created in the provider component so that the Context hook can access the objects it exposes
@@ -17,6 +15,7 @@ export const WorkoutList = () => {
     
     let currentUser = parseInt(sessionStorage.getItem(userStorageKey))
     let userWorkouts = workouts.filter(workout => currentUser === workout.userId)
+    // if the currentUsers id is 3 , return an array of all the workouts that have that userId
 
 
     const history = useHistory()
@@ -30,13 +29,12 @@ export const WorkoutList = () => {
       //  Within a React component, useEffect is a function. 
       // After the return, useEffect is automatically invoked and since the dependency array is empty, it only runs the first time the component renders. You can include dependencies in the array to cause the useEffect to run additional times.
 
-       // useEffect dependency array with dependencies - will run if dependency changes (state)
-
         // searchTerms will cause a change
         useEffect(() => {
           if (searchTerms !== "") {
             // If the search field is not blank, display matching workouts
             const subset = userWorkouts.filter(workout => workout.name.toLowerCase().includes(searchTerms.toLowerCase()))
+            // The toLowerCase() method returns the calling string value converted to lower case
             setFilteredWorkouts(subset)
           } else {
             // If the search field is blank, display all workouts
@@ -53,22 +51,18 @@ export const WorkoutList = () => {
         </button>
         </div>
        
-
         <div className="workouts">
           <div className="typeBtn">
           <button onClick={() => setFilteredWorkouts(userWorkouts.filter(workout=> workout.type === "Upper"))}>Upper</button>
           <button onClick={() => setFilteredWorkouts(userWorkouts.filter(workout=> workout.type === "Lower"))}>Lower</button>
-          </div>
-
-      
-          
+          </div>          
         {
         filteredWorkouts.map(workout => {
           return <WorkoutCard key={workout.id} workouts={workout} />
         })
         }
       </div>
-      {/* we use the .map() array method to iterate the array of workouts and generate HTML for each one by invoking the WorkoutCard component function */}
+      {/* we use the .map() array method to iterate the array of userWorkouts and generate HTML for each one by invoking the WorkoutCard component function */}
 
       {/* the 'key' and 'workouts' arguments become properties on an object that gets passed as an argument */}
       </>
